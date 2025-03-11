fe11d53e71f53966c2a156f01796c59fb2fb3558
ruben <rubenvarj@gmail.com>
2025-03-12 00:02:24 +0100

    creado controlador

---
+++ TravelTales/src/presentacion/controlador/Controlador.java
@@ -0,0 +1,12 @@
+package presentacion.controlador;
+
+public abstract class Controlador {
+	private static Controlador instancia;
+
+	public synchronized static Controlador getInstancia() {
+		if (instancia == null) instancia = new ControladorImp();
+		return instancia;
+	}
+
+	public abstract void accion(/*Context context*/);
+}
---
+++ TravelTales/src/presentacion/controlador/ControladorImp.java
@@ -0,0 +1,14 @@
+package presentacion.controlador;
+
+public class ControladorImp extends Controlador {
+
+	@Override
+	public void accion(/*Context context*/) {
+		//Cojo la instancia de la factoria de comandos
+		//Creo el comando
+		//if (command != null) 
+		// ejecuto el comando
+		//Cojo la instancia de la factoria de vistas 
+		//Actualizo la vista
+	}
+}
