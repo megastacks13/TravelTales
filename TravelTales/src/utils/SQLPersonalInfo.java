faeccaf9a4123d4bba4b6eb18e3ecd20124a473f
megastacks13 <jaialo04@ucm.es>
2025-03-11 15:15:39 +0000

    Idk

--- TravelTales/src/utils/SQLPersonalInfo.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public class SQLPersonalInfo {
-	public static final String URL = "jdbc:mysql://localhost:3306/TravelTales";
-    public static final String USERNAME = "root";
-    public static final String PASSWORD = "";
-}
