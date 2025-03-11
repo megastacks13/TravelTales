6aa8e5c2239cdac8db060da95660ec44c09a84de
megastacks13 <jaialo04@ucm.es>
2025-03-11 22:29:35 +0000

    Añadido test de prueba

---
+++ TravelTales/test/persistencia/dao/viaje/TestDAOViaje.java
@@ -0,0 +1,14 @@
+package persistencia.dao.viaje;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestDAOViaje {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
